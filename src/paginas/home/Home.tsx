d08876c7463017f0708986357c7749554d2a95ca
linefs <4pdublack@gmail.com>
2024-02-14 13:04:51 -0300

    React inicio

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+
+import './Home.css'
+
+function Home(){
+    return (
+    <>
+    <h1 className="titulo">Dev Moms in action</h1>
+    <img className="img" src="https://lh3.googleusercontent.com/pw/ABLVV84_WKgv568i-13cXIYp0Ug7H6SXrMFzWCaSYHKrK0RyPlX9V__2_gjcwxtPhQEF_LDXnoVJuResRuHvZ16WDZxdBLt6SaOSm4vyuQk9JkNy0AuIxr2wH31Je7PjK2P_8S6pqcMvU3i8SYhq7ihkCSS7Wha-fkKutiFQotc8XchB55xij05hGO6x08UodvuToginkUgYBncHFQB8DpXS0zrePf3rRlZ5ApfKyRubpDMhTAoekP47KDQj-5DjVDiRpSdWrD991yrRhBxhlL7ej2wxNXserSHM_Ptcg-1BnapgI0LrQzp4ZTL6q26Qkxc47BSzIH91wWK0izDE3xWMfcY23H4HZ4OPhkARxm7wHNZZbfBjQUfS_IzAh1eLTCA3l-MDZN5S0WlRI9dCEoNNdp16Cky7x3FtkRbhgyRyk60t5VEgdpua8SkdYfAUoGxIhSN69gyHDyUWG-6soRnw-tNwJoSN55oUin89lrcRzXsOmF3re5IqdU61kYpohqDJZVJ_JCU20EIsWZ8z5n8gHcBX-Oy49iq7zm89mPBGol9udOy_y68e3rOsvgqIwtnKfUm0uTFxF_kzqKbKIuY3xG-lnUX31evE3OZAd--tTIy2PFxPd3fW8kYUjoaGc3cUXaxC-KZ5bbsi3GuS6F2soPGwCniwuJCFQMrehV1jRfxXxJhDz-VMaPSSwt_da-OTjWqx2KZR9EOtqGk5Vjm5XW-GQURff83btLFqUquvg327g8ZZyygtihHK-aMFTsLp_wQWYz74B14qlixzI81mQyTSSy6QmJ2xCwIlQQklV3GWoB8xHJxFa_PWcAVuxiJ_0vmuXjl1NWLez_5SRz1r_bz3dhiP7g2Ut_rq-WWN74bUEUEQCaxiENfHkqTn0JIOoLqiFwJXuoCHPHki3HVDUB-zgA=w608-h608-s-no-gm?authuser=0" alt="Imagem logo Dev moms"></img>
+    </>
+    )
+}
+
+export default Home;
+
